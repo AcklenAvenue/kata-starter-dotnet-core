c5c0642fa15c99feaba7f0781a4ed5364e810955
Byron Sommmmardahl <byron@acklenavenue.com>
2019-01-26 13:25:03 -0600

    Initial commit.

---
+++ src/Kata.Spec/when_attempting_to_change_a_mutable_string.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using Machine.Specifications;
+
+namespace Kata.Spec
+{
+    public class when_adding_two_numbers
+    {
+        static int _sut;
+
+        Establish context = () => { 
+            _sut = 1;
+        };
+        
+        Because action = () => {            
+            _sut = _sut+1; 
+        };
+
+        It should_return_the_sum = () => _sut.Should().Be(2);
+    }
+}
