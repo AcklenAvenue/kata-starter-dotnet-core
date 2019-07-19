a611c6a818f5fcb1a71b6b6677dd1a2a329e04ff
Byron Sommardahl <byron@acklenavenue.com>
2019-07-18 19:26:31 -0500

    Improving the first test.

--- src/Kata.Spec/when_calculating_the_sum/when_attempting_to_change_a_mutable_string.cs
+++ src/Kata.Spec/when_calculating_the_sum/when_attempting_to_change_a_mutable_string.cs
@@ -5,13 +5,16 @@ namespace Kata.Spec.when_calculating_the_sum
 {
     public class when_the_user_input_is_empty
     {
-        static Calculator _sut;
+        static Calculator _systemUnderTest;
         static int _result;
 
-        Establish context = () => _sut = new Calculator();
+        Establish context = () => 
+            _systemUnderTest = new Calculator();
 
-        Because of = () => _result = _sut.Add("");
+        Because of = () => 
+            _result = _systemUnderTest.Add("");
 
-        It should_return_zero = () => _result.Should().Be(0);
+        It should_return_zero = () => 
+            _result.Should().Be(0);
     }
 }
