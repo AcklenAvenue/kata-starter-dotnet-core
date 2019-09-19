d45c771f2afecbc1b10098f385d961872eafd531
Byron Sommardahl <byron@acklenavenue.com>
2019-09-19 10:49:45 -0500

    Empty string passing, one number failing.

--- src/Kata.Spec/when_adding_the_third_char_from_each_line.cs
+++ src/Kata.Spec/when_adding_the_third_char_from_each_line.cs
@@ -13,4 +13,18 @@ namespace Kata.Spec
         static Calculator _systemUnderTest;
         static int _result;
     }
+
+    public class when_user_input_is_one_number
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("1"); };
+
+        It should_return_that_number = () => { _result.Should().Be(1); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
 }
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -2,9 +2,9 @@ namespace Kata
 {
     public class Calculator
     {
-        public int Add()
+        public int Add(string s = "")
         {
-            return -1;
+            return 0;
         }
     }
 }
